4add881333db200ae9b9e62cc09bd8c48a5aeaeb
mazen sallam <mazensallam75@gmail.com>
2022-03-21 03:15:32 +0200

    added functionallity to state and it can generate neighbour states now

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,14 +4,19 @@ import java.util.*;
 public class Main {
 	private  static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) {
-		 byte[] start = new byte[9];
+//		 byte[] start = new byte[9];
+		byte[] start = {1, 5, 3, 2, 6, 4, 7, 8, 0};
          byte[] goal = {0,1,2,3,4,5,6,7,8};
+
+         State state = new State(start, new Heuristics(1));
+         state.getNeighbours();
+//		System.out.println(state.getKey());
           
-         System.out.println("Enter Puzzle:");
-         for(int i= 0 ; i < 9 ; i++)
-             start[i] = input.nextByte();
-        
-         algorithm_choice(start, goal);
+//         System.out.println("Enter Puzzle:");
+//         for(int i= 0 ; i < 9 ; i++)
+//             start[i] = input.nextByte();
+//
+//         algorithm_choice(start, goal);
          
 	}
    public static void algorithm_choice (byte[] start , byte[] goal)
@@ -23,31 +28,31 @@ public class Main {
 	   System.out.printf("Choose algorithm\n1)BFS\n2)DFS\n3)A*\n>>");
 	   byte choice=input.nextByte() ;
 	  
-	   switch (choice)
-       {
-           case 1:
-               System.out.println("\n---------- BFS ----------");
-               heuristic= new Heuristics(goal , 0);
-               s= new State(start,heuristic);
-               BFS bfs = new BFS(s);
-               bfs.BFS_search();
-               break;
-           case 2:
-        	   System.out.println("\n---------- DFS ----------");
-        	  heuristic= new Heuristics(goal , 0);
-        	   s= new State(start,heuristic);
-        	   DFS dfs = new DFS(s);
-        	   dfs.DFS_search();
-        	   break;
-           case 3:
-        	   System.out.println("\n---------- A* ----------");
-        	   System.out.printf("choose heuristic\n1)Manhattan\n2)Euclidean\n>>");
-        	   choice=input.nextByte();
-        	   heuristic= new Heuristics(goal ,choice);
-           	   s= new State(start,heuristic);
-           	A_STAR a_star= new A_STAR(s);
-           	// method to solve a_star
-           	   break;
-       }
+//	   switch (choice)
+//       {
+//           case 1:
+//               System.out.println("\n---------- BFS ----------");
+//               heuristic= new Heuristics(goal , 0);
+//               s= new State(start,heuristic);
+//               BFS bfs = new BFS(s);
+//               bfs.BFS_search();
+//               break;
+//           case 2:
+//        	   System.out.println("\n---------- DFS ----------");
+//        	  heuristic= new Heuristics(goal , 0);
+//        	   s= new State(start,heuristic);
+//        	   DFS dfs = new DFS(s);
+//        	   dfs.DFS_search();
+//        	   break;
+//           case 3:
+//        	   System.out.println("\n---------- A* ----------");
+//        	   System.out.printf("choose heuristic\n1)Manhattan\n2)Euclidean\n>>");
+//        	   choice=input.nextByte();
+//        	   heuristic= new Heuristics(goal ,choice);
+//           	   s= new State(start,heuristic);
+//           	A_STAR a_star= new A_STAR(s);
+//           	// method to solve a_star
+//           	   break;
+//       }
    }
 }
--- src/app/State.java
+++ src/app/State.java
@@ -1,11 +1,33 @@
 package app;
 
+import java.util.ArrayList;
+
 public class State {
+
+	private class Pos{
+		public Pos(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		int x;
+		int y;
+	}
     private int  Hn,depth;
     private String path;
     private Heuristics heuristic;
     private byte[] state;
     
+
+    public String getKey(byte[] state) {
+    	String str = "";
+    	for(int i = 0; i < state.length; i++) {
+
+    		str += String.valueOf(state[i]);
+		}
+
+    	return str;
+	}
 
 	public State(byte[] start, Heuristics h) {
       this(start,0,"0->",h );
@@ -34,6 +56,70 @@ public class State {
     	byte[] g=heuristic.getGoal();
     	return(state[0]==g[0] && state[1]==g[1] && state[2]==g[2] && state[3]==g[3] && state[4]==g[4] && state[5]==g[5] && state[6] == g[6] && state[7]==g[7] && state[8]==g[8]);
     }
+
+    private int getZeroPos() {
+    	for(int i = 0; i < state.length; i++) {
+    		if(state[i] == 0)
+    			return i;
+		}
+
+    	return -1;
+	}
+
+	private byte[] swap(byte[] state, int i, int j) {
+    	byte[] newState = state.clone();
+    	byte temp = newState[i];
+    	newState[i] = newState[j];
+    	newState[j] = temp;
+		return newState;
+	}
+
+	private int convertXYtoX(int x, int y, int n) {
+    	return n*x+y;
+	}
+
+	private byte[][] convertTo2D(byte[] oneD, int n) {
+		byte[][] twoD = new byte[3][3];
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				twoD[i][j] = oneD[i*3+j];
+			}
+		}
+		return twoD;
+	}
+
+    public ArrayList<byte[]> getNeighbours(){
+    	byte[][] tiles = new byte[3][3];
+		ArrayList<byte[]> neighbours = new ArrayList<>();
+		int zero = getZeroPos();
+		tiles = convertTo2D(state, 3);
+		int x = zero%3;
+		int y = (int) (Math.ceil((double)zero/3)-1);
+
+		ArrayList<Pos> positions = new ArrayList<>();
+
+		if(x+1 < 3) {
+			positions.add(new Pos(x+1, y));
+		}
+		if(x-1 > 0) {
+			positions.add(new Pos(x-1, y));
+		}
+		if(y+1 < 3) {
+			positions.add(new Pos(x, y+1));
+		}
+		if(y-1 > 0) {
+			positions.add(new Pos(x, y-1));
+		}
+
+		for(Pos pos: positions) {
+			int i = convertXYtoX(pos.x, pos.y, 3);
+			byte[] neighbour = swap(state, i, zero);
+			System.out.println(getKey(neighbour));
+			neighbours.add(neighbour);
+		}
+
+		return neighbours;
+	}
     
 	public int getDepth() {
 		return depth;
