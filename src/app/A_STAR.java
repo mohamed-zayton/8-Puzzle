6ffdaa9f2ccf47f5b55465d5aa0caa121310f266
Mohamed Medhat Saeed Hamada Zayton <es-mohamed.medhat2019@alexu.edu.eg>
2022-03-21 19:57:42 +0200

    a-star

--- src/app/A_STAR.java
+++ src/app/A_STAR.java
@@ -1,9 +1,38 @@
 package app;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+
 public class A_STAR {
 
-	public A_STAR(State s) {
+	public A_STAR(State s, int heu) {
 		// TODO Auto-generated constructor stub
+		PriorityQueue<State> frontier = new PriorityQueue<>();
+		HashSet<String> explored = new HashSet<String>();
+		HashMap<String,String> childParent = new HashMap<>();
+		frontier.add(s);
+
+
+		while (!frontier.isEmpty()){
+			var state = frontier.poll();
+			explored.add(state.getKey());
+
+			if (state.is_Goal()){
+				break;
+			}
+			for (var ne: state.getNeighbours()) {
+				var newState = new State(ne, new Heuristics(heu));
+				if (!explored.contains(ne)) {
+					frontier.add(newState);
+				}else {
+					var value = state.getDepth() + newState.getDepth();
+
+				}
+			}
+
+
+		}
 	}
 
 }
