c350da9d31dda84afbf5a45a8d62bdab0b90414d
Mostafa Tawfiq <mostafa.mohammad.twfiq@gmail.com>
2022-03-24 19:53:41 +0200

    Finished Refactoring the code.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,8 @@
 package app;
+import app.Algorithms.A_STAR;
+import app.Algorithms.BFS;
+import app.Algorithms.DFS;
+
 import java.util.*;
 
 public class Main {
@@ -13,9 +17,37 @@ public class Main {
 	    for (State n : neighbors)
 			System.out.println(n);
 	    //TODO: Solve the puzzle here.*/
+		int initialState = 867254301;
+		IntState intState = new IntState();
+		/*if(puzzleIsSolvable(initialState))
+			System.out.println("Solvable");
+		else
+			System.out.println("Not Solvable");
+*/
+		//8 6 7
+		// 2 5 4
+		// 3 . 1
+		A_STAR a = new A_STAR();
+		List<Integer> path = a.AStar(initialState, IntState.HeuristicsType.EUCLIDEAN);
+		System.out.println(path.size());
+		for (int n : path)
+			System.out.println(intState.intStateToString(n));
 
-		
 
+   }
+
+   private static boolean puzzleIsSolvable(int initialState) {
+		byte[] state = new IntState().intStateToByteArr(initialState);
+	   int inv_count = 0;
+	   for (int i = 0; i < 3 - 1; i++) {
+		   for (int j = i + 1; j < 3; j++) {
+			   // Value 0 is used for empty space
+			   if (state[j * 3 + i] > 0 && state[j * 3 + i] > state[i * 3 + j])
+				   inv_count++;
+		   }
+	   }
+
+	   return inv_count%2 == 0;
    }
 
    private static byte[] scanStartState(int boardSize) {
